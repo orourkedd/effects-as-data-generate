e83134d5a34d88a99ffb776ba310253192a52ab7
psfrankie <frankie-orourke@pluralsight.com>
2017-05-11 11:50:40 -0700

    add help by default

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const fxns = require('./effects');
 const chalk = require('chalk');
 const { failure, isFailure } = require('effects-as-data');
 
-program.version('0.0.1').command('init').action(function() {
-  fxns.init().then(printResult).catch(printFailure);
-});
+program
+  .version('0.0.1')
+  .command('init', 'initialize an effects-as-data project')
+  .action(function() {
+    fxns.init().then(printResult).catch(printFailure);
+  });
 
-program.version('0.0.1').command('fn').action(function(fn) {
-  const fnName = typeof fn === 'string' ? fn : null;
-  fxns.newFn(fnName).then(printResult).catch(printFailure);
-});
+program
+  .version('0.0.1')
+  .command('fn', 'create new effects-as-data function')
+  .action(function(fn) {
+    const fnName = typeof fn === 'string' ? fn : null;
+    fxns.newFn(fnName).then(printResult).catch(printFailure);
+  });
 
 function printResult(result) {
   if (isFailure(result)) printFailure(result);
@@ -30,3 +36,5 @@ function printFailure(result) {
 }
 
 program.parse(process.argv);
+
+if (!process.argv.length) program.outputHelpInformation();
